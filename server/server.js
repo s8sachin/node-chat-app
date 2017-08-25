3538b59472556275e3005b2fa1ef16e5cb27b0dd
Sachin Srinivasan <ssampathkumar@qwinix.io>
2017-08-25 22:20:55 +0530

    greet new user and alert

--- server/server.js
+++ server/server.js
@@ -13,14 +13,33 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcome to chat app",
+    createdAt: new Date().getTime
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: "Admin",
+    text: "New user connected to chat.",
+    createdAt: new Date().getTime
+  })
 
   socket.on('createMessage', (message) =>{
     console.log('createMessage', message);
+
+    //socket.emit from admin, text welcome
+    //socket.broadcast from admin, new user joined
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     })
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime
+    // });
   });
 
   socket.on('disconnect', () => {
